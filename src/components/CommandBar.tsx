024ea013b9d67ed6b823d34072b690e412a36f21
mjocc <61629073+mjocc@users.noreply.github.com>
2022-07-12 21:21:14 +0100

    Update command bar positioning to not cover sidebar information

--- src/components/CommandBar.tsx
+++ src/components/CommandBar.tsx
@@ -52,110 +52,113 @@ const CommandBar: FC = () => {
     <>
       <Collapse in={openBar} onExited={() => setOpenButton(true)}>
         <Container
-          style={{ zIndex: 1000 }}
+          style={{ zIndex: 1000, paddingLeft: '16.666667%' }}
           className="position-fixed bottom-0 start-0 end-0"
+          fluid
         >
-          <div
-            className="px-4 pt-3 pb-1 m-2 mb-4 rounded-3 border border-primary bg-light"
-            style={{ '--bs-bg-opacity': 0.8 } as CSSProperties}
-          >
-            <Form onSubmit={handleSubmit}>
-              <Row>
-                <Col xs={2}>
-                  <Form.Group controlId="command-bar-method-field">
-                    <ToggleButtonGroup
-                      type="radio"
-                      name="http-method"
-                      aria-label="http method"
-                      value={method}
-                      onChange={(value) => setMethod(value)}
-                      className="d-flex"
-                    >
-                      <ToggleButton
-                        variant="outline-primary"
-                        id="get-button"
-                        value="GET"
+          <div className="position-relative">
+            <div
+              className="px-4 pt-3 pb-1 m-2 mb-4 rounded-3 border border-primary bg-light"
+              style={{ '--bs-bg-opacity': 0.8 } as CSSProperties}
+            >
+              <Form onSubmit={handleSubmit}>
+                <Row>
+                  <Col xs={2}>
+                    <Form.Group controlId="command-bar-method-field">
+                      <ToggleButtonGroup
+                        type="radio"
+                        name="http-method"
+                        aria-label="http method"
+                        value={method}
+                        onChange={(value) => setMethod(value)}
+                        className="d-flex"
                       >
-                        GET
-                      </ToggleButton>
-                      <ToggleButton
-                        variant="outline-primary"
-                        id="post-button"
-                        value="POST"
-                      >
-                        POST
-                      </ToggleButton>
-                      <ToggleButton
-                        variant="outline-primary"
-                        id="put-button"
-                        value="PUT"
-                      >
-                        PUT
-                      </ToggleButton>
-                    </ToggleButtonGroup>
-                    <Form.Label>Method</Form.Label>
-                  </Form.Group>
-                </Col>
-                <Col xs={method !== 'GET' ? 4 : 8}>
-                  <Form.Group controlId="command-bar-path-field">
-                    <InputGroup>
-                      <ApiDocsPopover>
-                        <InputGroup.Text id="api-url-prefix">
-                          api.spacetraders.io
-                        </InputGroup.Text>
-                      </ApiDocsPopover>
-                      <Form.Control
-                        type="text"
-                        aria-describedby="api-url-prefix"
-                        placeholder="e.g. /game/status"
-                        value={path}
-                        onChange={handleFormChange(setPath)}
-                      />
-                    </InputGroup>
-                    <Form.Label>Path</Form.Label>
-                  </Form.Group>
-                </Col>
-                {method !== 'GET' && (
-                  <Col xs={4}>
-                    <Form.Group controlId="command-bar-body-field">
-                      <Form.Control
-                        type="text"
-                        placeholder='e.g. { "hello": "world" }'
-                        value={body}
-                        onChange={handleFormChange(setBody)}
-                      />
-                      <Form.Label>Request body (JSON)</Form.Label>
+                        <ToggleButton
+                          variant="outline-primary"
+                          id="get-button"
+                          value="GET"
+                        >
+                          GET
+                        </ToggleButton>
+                        <ToggleButton
+                          variant="outline-primary"
+                          id="post-button"
+                          value="POST"
+                        >
+                          POST
+                        </ToggleButton>
+                        <ToggleButton
+                          variant="outline-primary"
+                          id="put-button"
+                          value="PUT"
+                        >
+                          PUT
+                        </ToggleButton>
+                      </ToggleButtonGroup>
+                      <Form.Label>Method</Form.Label>
                     </Form.Group>
                   </Col>
-                )}
-                <Col xs={2}>
-                  <Stack>
-                    <ButtonGroup style={{ flex: 1 }}>
-                      <SubmitButton
-                        style={{ flex: 1 }}
-                        submitting={submitting}
-                      />
-                      <Link href="/command/history" passHref>
-                        <Button style={{ flex: 1 }} variant="secondary">
-                          History
-                        </Button>
-                      </Link>
-                    </ButtonGroup>
-                  </Stack>
-                </Col>
-              </Row>
-            </Form>
-          </div>
-          <div className="position-fixed end-0 bottom-0 pb-4 d-flex justify-content-center align-items-center w-100">
-            <Tooltip tooltipText="Hide command bar">
-              <ChevronDown
-                role="button"
-                size={30}
-                onClick={() => {
-                  setOpenBar(false);
-                }}
-              />
-            </Tooltip>
+                  <Col xs={method !== 'GET' ? 4 : 8}>
+                    <Form.Group controlId="command-bar-path-field">
+                      <InputGroup>
+                        <ApiDocsPopover>
+                          <InputGroup.Text id="api-url-prefix">
+                            api.spacetraders.io
+                          </InputGroup.Text>
+                        </ApiDocsPopover>
+                        <Form.Control
+                          type="text"
+                          aria-describedby="api-url-prefix"
+                          placeholder="e.g. /game/status"
+                          value={path}
+                          onChange={handleFormChange(setPath)}
+                        />
+                      </InputGroup>
+                      <Form.Label>Path</Form.Label>
+                    </Form.Group>
+                  </Col>
+                  {method !== 'GET' && (
+                    <Col xs={4}>
+                      <Form.Group controlId="command-bar-body-field">
+                        <Form.Control
+                          type="text"
+                          placeholder='e.g. { "hello": "world" }'
+                          value={body}
+                          onChange={handleFormChange(setBody)}
+                        />
+                        <Form.Label>Request body (JSON)</Form.Label>
+                      </Form.Group>
+                    </Col>
+                  )}
+                  <Col xs={2}>
+                    <Stack>
+                      <ButtonGroup style={{ flex: 1 }}>
+                        <SubmitButton
+                          style={{ flex: 1 }}
+                          submitting={submitting}
+                        />
+                        <Link href="/command/history" passHref>
+                          <Button style={{ flex: 1 }} variant="secondary">
+                            History
+                          </Button>
+                        </Link>
+                      </ButtonGroup>
+                    </Stack>
+                  </Col>
+                </Row>
+              </Form>
+            </div>
+            <div className="position-absolute end-0 bottom-0 d-flex justify-content-center align-items-center w-100">
+              <Tooltip tooltipText="Hide command bar">
+                <ChevronDown
+                  role="button"
+                  size={30}
+                  onClick={() => {
+                    setOpenBar(false);
+                  }}
+                />
+              </Tooltip>
+            </div>
           </div>
         </Container>
       </Collapse>
